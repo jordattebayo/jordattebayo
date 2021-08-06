9b095422ceaec1618dd98e439916139cb78a2c57
Jordan Booker <jordan.m.booker@gmail.com>
2021-08-05 21:08:34 -0400

    Added Project, style fixes
    Fixed styles for the projects card, on hover the card would expand.
    Added amplifier project. Fixed inconsistent padding for footer.

--- jordattebayo/components/Resume.js
+++ jordattebayo/components/Resume.js
@@ -1,17 +1,19 @@
-import styles from "./resume.module.sass";
+import styles from './resume.module.sass'
 
 export default function Resume() {
   return (
     <div className={styles.container}>
       <div className={styles.resContainer}>
-        <p className={styles.resText}>Somebody hire this man:</p>
+        <p className={styles.resText}>
+          Want to find out more, but in a way that's less fun?
+        </p>
         <a
-          href="/Jordan_Booker_Website_Resume.pdf"
-          download="Jordan_Booker_Resume"
+          href='/Jordan_Booker_Website_Resume.pdf'
+          download='Jordan_Booker_Resume'
         >
           <button className={styles.btn}>Download Resume</button>
         </a>
       </div>
     </div>
-  );
+  )
 }
