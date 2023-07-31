9c41772407ac90f11ac854d7ac7d589f020135c3
Jordan Booker <jordan.m.booker@gmail.com>
2023-07-30 23:18:21 -0400

    Progress on nav menu styling

--- components/blog-nav.tsx
+++ components/blog-nav.tsx
@@ -1,10 +1,16 @@
 import { useState } from "react";
+import Link from "next/link";
 import styled from "styled-components";
 
 const MenuContainer = styled.div`
-  border: 1px solid ${({theme}) => theme.colors.primary};
-  position: relative;
-  @media(max-width: ${(props) => props.theme.widths.tablet}) {
+  border-bottom: 3px solid ${({theme}) => theme.colors.primary};
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  top: 272px;
+  z-index: 1;
+  @media(max-width: ${(props: any) => props.theme.widths.tablet}) {
     display: none;
   }
 `
@@ -12,6 +18,47 @@ const MenuContainer = styled.div`
 const MenuContents = styled.ul<{open: boolean}>`
   display: ${({open}) => open ? "unset" : "none"};
   position: absolute;
+  background-color: white;
+  bottom: 0;
+`
+
+const MenuLink = styled.a`
+  text-decoration: none;
+`
+
+const MenuToggleButton = styled.button`
+  background-color: transparent;
+  border: none;
+  &:hover{
+    cursor: pointer;
+  }
+`
+
+const MenuIcon = styled.span`
+  background-color: #FF6831;
+  display: inline-block;
+  position: relative;
+  z-index: 2;
+  top: 20px;
+  left: 20px;
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+`
+
+const LinkList = styled.ul`
+  display: flex;
+  flex-direction: row;
+  padding: 0;
+  margin: 0;
+`
+
+const LinkListItem = styled.li`
+  list-style: none;
+  padding-left: 1rem;
+  &.first {
+    padding-left: 0;
+  }
 `
 
 export default function BlogNav(){
@@ -29,13 +76,28 @@ export default function BlogNav(){
           <p>
             Sign up form here
           </p>
-          <ul>
-            <li>RSS</li>
-            <li>About</li>
-            <li>Portfolio</li>
-          </ul>
+          <LinkList>
+            <LinkListItem className="first">
+              <Link href="/rss/feed.xml" passHref legacyBehavior>
+                <MenuLink>/rss</MenuLink>
+              </Link>
+            </LinkListItem>
+            <LinkListItem>
+              <Link href="/about" passHref legacyBehavior>
+                <MenuLink>/about</MenuLink>
+              </Link>
+            </LinkListItem>
+            <LinkListItem>
+              <Link href="/#portfolio" passHref legacyBehavior>
+                <MenuLink>/portfolio</MenuLink>
+              </Link>
+            </LinkListItem>
+          </LinkList>
         </MenuContents>
-        <button onClick={toggleMenu}>Open</button>
+        <MenuToggleButton onClick={toggleMenu}>
+          Menu
+          <MenuIcon></MenuIcon>
+        </MenuToggleButton>
       </MenuContainer>
     )
 }
