91fe99deecf3f03e43ff32a5fbf5d538cf000a74
Jordan Booker <jordanmbooker@outlook.com>
2023-08-07 22:48:20 -0400

    good progress on styling for nav, finally slides in and out. dialing in lengths for bottom lines.

--- components/blog-nav.tsx
+++ components/blog-nav.tsx
@@ -3,33 +3,56 @@ import Link from "next/link";
 import styled from "styled-components";
 
 const MenuContainer = styled.div`
-  border-bottom: 3px solid ${({theme}) => theme.colors.primary};
+  border-bottom: 3px solid ${({ theme }) => theme.colors.primary};
   position: absolute;
   display: flex;
   flex-direction: column;
   align-items: start;
   top: 272px;
   z-index: 1;
+
+`
+
+const StepsWrapper = styled.div`
+  position: absolute;
+  top: 325px;
   @media(max-width: ${(props: any) => props.theme.widths.tablet}) {
     display: none;
   }
 `
+// display: ${({open}) => open ? "unset" : "none"};
 
-const MenuContents = styled.ul<{open: boolean}>`
-  display: ${({open}) => open ? "unset" : "none"};
+const MenuContents = styled.ul<{ open: boolean }>`
+  padding: unset;
   position: absolute;
   background-color: transparent;
   bottom: 0;
+  right: ${({ open }) => open ? "-300px" : "-80px"};
+  transition: all .2s ease-in-out;
+`
+
+const MenuItem = styled.li`
+  padding-left: 1rem;
+  margin-bottom: auto;
+  border-bottom: 3px solid black;
+  height: 50px;
 `
 
 const MenuLink = styled.a`
   text-decoration: none;
 `
 
+const MenuText = styled.p`
+  margin-top: auto;
+`
+
 const MenuToggleButton = styled.button`
+
   background-color: transparent;
   border: none;
-  &:hover{
+  display: flex;
+  flex-direction: row;
+  &:hover {
     cursor: pointer;
   }
 `
@@ -60,61 +83,26 @@ const LinkListItem = styled.li`
     padding-left: 0;
   }
 `
-
-/* theme steps */
-interface ThemeShapeProps {
-  bottomAligned?: boolean;
-  filled?: boolean;
-}
-
-const StepsWrapper = styled.div`
-  position: absolute;
-  padding-bottom: 4rem;
-  margin-top: auto;
-  background-color:${(props) => props.theme.colors.senary};
-  @media(max-width: ${(props) => props.theme.widths.tablet}) {
-      display: none;
+export default function BlogNav() {
+  const [openMenu, setOpenMenu] = useState<boolean>(false);
+  const toggleMenu = () => {
+    console.log(openMenu)
+    setOpenMenu(!openMenu)
   }
-`
-
-const Step = styled.div`
-  border-bottom: 3px solid ${({theme}) => theme.colors.primary};
-  height: 55px;
-  right: 0;
-`
-
-const StepOne = styled(Step)`
-  width: 20%;
-  top: 0;
-`
-
-const StepTwo = styled(Step)`
-  width: 15%;
-  top: 55px;
-`
-
-const StepThree = styled(Step)`
-  width: 10%;
-  top: 110px;
-`
-
-export default function BlogNav(){
-    const [openMenu, setOpenMenu] = useState<boolean>(false);
-    const toggleMenu = () => {
-          console.log(openMenu)
-          setOpenMenu(!openMenu)
-    }
-    return (
-        <StepsWrapper>
-          <StepOne></StepOne>
-          <StepTwo></StepTwo>
-        <MenuContents open={openMenu}>
-            <p>
-              Theme toggle here
-            </p>
-            <p>
-              Sign up form here
-            </p>
+  return (
+    <StepsWrapper>
+      <MenuContents open={openMenu}>
+        <MenuItem>
+          <MenuText>
+            Theme toggle here
+          </MenuText>
+        </MenuItem>
+        <MenuItem>
+          <MenuText>
+            Sign up form here
+          </MenuText>
+        </MenuItem>
+        <MenuItem>
           <LinkList>
             <LinkListItem className="first">
               <Link href="/rss/feed.xml" passHref legacyBehavior>
@@ -131,14 +119,16 @@ export default function BlogNav(){
                 <MenuLink>/portfolio</MenuLink>
               </Link>
             </LinkListItem>
+            <LinkListItem>
+              <MenuToggleButton onClick={toggleMenu}>
+                Menu
+                <MenuIcon>
+                </MenuIcon>
+              </MenuToggleButton>
+            </LinkListItem>
           </LinkList>
-        </MenuContents>
-        <StepThree>
-        <MenuToggleButton onClick={toggleMenu}>
-          Menu
-          <MenuIcon></MenuIcon>
-        </MenuToggleButton>
-        </StepThree>
-      </StepsWrapper>
-    )
+        </MenuItem>
+      </MenuContents>
+    </StepsWrapper>
+  )
 }
