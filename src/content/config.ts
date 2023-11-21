19f3c0b8b28311e9c6df0fa2b94c36e0779f6fab
Jordan Booker <jordan.m.booker@gmail.com>
2023-11-21 10:24:10 -0500

    Replaced nextJS with Astro, added basic skeleton structure of blog, added basic content

---
+++ src/content/config.ts
@@ -0,0 +1,24 @@
+import { defineCollection, z } from 'astro:content';
+
+const blog = defineCollection({
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		excerpt: z.string().optional(),
+		heroImage: z.object({
+			path: z.string(),
+			alt: z.string()
+		}),
+		description: z.string(),
+		// Transform string to Date object
+		pubDate: z.coerce.date(),
+		updatedDate: z.coerce.date().optional(),
+		ogImage: z.object({
+			path: z.string(),
+			alt: z.string()
+		}),
+		lead: z.string().optional()
+	}),
+});
+
+export const collections = { blog };
