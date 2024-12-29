232fbc76210cbf2058c5c0e09e7960bcbfe9794f
jordan <jordan.m.booker@gmail.com>
2024-12-28 21:09:58 -0500

    Added first blog post, first journal entry, minor updates to journal link styling

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,25 +1,47 @@
 import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		excerpt: z.string().optional(),
-		heroImage: z.object({
-			path: z.string(),
-			alt: z.string()
-		}),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		ogImage: z.object({
-			path: z.string(),
-			alt: z.string()
-		}),
-		lead: z.string().optional(),
-		isDraft: z.boolean().default(false)
-	}),
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    excerpt: z.string().optional(),
+    heroImage: z.object({
+      path: z.string(),
+      alt: z.string(),
+    }),
+    description: z.string(),
+    // Transform string to Date object
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    ogImage: z.object({
+      path: z.string(),
+      alt: z.string(),
+    }),
+    lead: z.string().optional(),
+    isDraft: z.boolean().default(false),
+  }),
 });
 
-export const collections = { blog };
+const journal = defineCollection({
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    excerpt: z.string().optional(),
+    heroImage: z.object({
+      path: z.string(),
+      alt: z.string(),
+    }),
+    description: z.string(),
+    // Transform string to Date object
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    ogImage: z.object({
+      path: z.string(),
+      alt: z.string(),
+    }),
+    lead: z.string().optional(),
+    isDraft: z.boolean().default(false),
+  }),
+});
+
+export const collections = { blog, journal };
---
+++ src/content/journal/hello-world.md
@@ -0,0 +1,17 @@
+---
+title: 'hello world 🎉'
+description: 'My first journal entry'
+pubDate: 'Dec 28 2024'
+heroImage:
+  path: ''
+  alt: ''
+ogImage:
+  path: ''
+  alt: ''
+isDraft: false
+---
+Wow, how incredible is it to be able to put your thoughts out into the world? Writing this—my very first journal entry—gives me the same elatement I felt when I created my first HTML page. There's a sense of wonder in seeing text that is marked up with special characters and tags, transform into something visually organized and beautiful in the browser. And it’s not just on my machine—it can be shared almost instantaneously with people around the world. A document available to anyone curious enough to request it. That’s exactly what this is: a letter to the world.
+
+I’ll keep it brief. My plans for this journal aren’t to share my deepest, darkest secrets for anyone to stumble upon. Sorry if you’re the type who enjoys snooping through people’s drawers! Instead, this is where I’ll post my more random and unfiltered thoughts. Expect musings on the anime I’m watching, the games I’m playing, the books I’m reading, or the things I’m creating. On that note, thanks for stopping by.
+
+![Monkey D Luffy Gif](https://media1.tenor.com/m/fwrky34J6o8AAAAd/luffy-monkey-d-luffy.gif)
